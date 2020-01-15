d154a0e1b91611876ff02e1bb879607492cc1ec9
agalligani <anthony.galligani@gmail.com>
2020-01-15 14:47:49 -0500

    final form - works great

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import { Text, View } from "react-native";
 
 import * as Font from "expo-font";
 
-export default class App extends React.Component {
-  componentDidMount() {
-    Font.loadAsync({
+export default class App extends Component {
+  state = {
+    fontLoaded: false
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
       "Dhurati-Regular": require("./assets/fonts/Dhurjati-Regular.ttf"),
-      "Roboto-Black": require("./assets/fonts/Roboto/Roboto-Black.ttf"),
+      "Roboto-Medium": require("./assets/fonts/Roboto/Roboto-Medium.ttf"),
+      "Roboto-Regular": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
       "Roboto-Thin": require("./assets/fonts/Roboto/Roboto-Thin.ttf")
     });
+
+    this.setState({ fontLoaded: true });
   }
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={{ fontFamily: "Roboto-Black", fontSize: 56 }}>
-          PUXXLER
-        </Text>
+        {this.state.fontLoaded ? (
+          <Text style={{ fontFamily: "Roboto-Regular", fontSize: 56 }}>
+            PUXXLER
+          </Text>
+        ) : null}
       </View>
     );
   }
